38814e4e1c3a0e1f7eb6d65360c2fcf7eb07d62d
ucs198604 <37744685+ucs198604@users.noreply.github.com>
2020-07-04 02:58:29 +0800

    check if duty reservation > assigned

--- schedule/test.py
+++ schedule/test.py
@@ -1,10 +1,24 @@
-# test for arguments
+# for obtaining arguments
 from argparse import ArgumentParser
-parser = ArgumentParser()
-parser.add_argument('-f', '--filename', dest = 'filename', default='test.xlsm')
-parser.add_argument('-n', '--number', dest = 'num', default = 100)
+parser = ArgumentParser(prog='Duty arrange',
+                        description='Arranging on-call duties for residents',
+                        epilog='please contact 4437 if any suggestions')
+
+parser.add_argument('-f', dest = 'filename', default='duty.xlsm', type=str) # to contain system arguments from jupyter
+parser.add_argument('-file', dest = 'filename', default='duty.xlsm', type=str)
+parser.add_argument('-num', dest = 'num', default = 100, type=int)
 
 args = parser.parse_args()
 
-print('filename = ', args.filename)
-print('number = ', args.num)
+# take argument of how many nunmbers to run
+# take argument of the file name, append xlsm if not
+NUM_TO_RUN = args.num
+FILE_NAME = args.filename
+FILE_NAME = 'duty.xlsm' if FILE_NAME.endswith('json') else FILE_NAME
+FILE_NAME = FILE_NAME if FILE_NAME.endswith('.xlsm') else FILE_NAME + '.xlsm'
+# if FILE_NAME.endswith('json') == True:
+#     FILE_NAME = 'duty.xlsm'
+# if FILE_NAME.endswith('.xlsm') == False:
+#     FILE_NAME = FILE_NAME + '.xlsm'
+
+
